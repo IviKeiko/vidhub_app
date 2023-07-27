afc500996501fcf10f67eeb5560c23e5fbacfff2
IviKeiko <jooly.klee@gmail.com>
2023-07-27 22:50:41 +0200

    videos are displayed on the feed

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,5 +1,22 @@
+import { Stack, Box } from "@mui/material";
+import { nanoid } from "nanoid";
+import { VideoCard, ChannelCard } from "./";
+
+Stack;
+
 function Videos({ videos }) {
   console.log(videos);
-  return <div>Videos</div>;
+  return (
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((video) => {
+        return (
+          <Box key={nanoid()}>
+            {video.id.videoId && <VideoCard video={video} />}
+            {/* {video.id.channelId && <ChannelCard channelDetail={video} />} */}
+          </Box>
+        );
+      })}
+    </Stack>
+  );
 }
 export default Videos;
